700d54ce3ce5a7785c6cd03a8915c4c155f9dc18
Aryan Kondhalkar <aryankondhalkar16@gmail.com>
2026-07-29 22:00:24 +0530

    feat: add rate limiting middleware and improve API security

--- server/src/app.js
+++ server/src/app.js
@@ -2,21 +2,40 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
+
 import authRouter from "./routes/Auth.route.js";
 import taskRouter from "./routes/Task.route.js";
 
 export const app = express();
+
 app.use(helmet());
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
   }),
 );
+
 app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Correct limiter setup
+const limiter = rateLimit({
+  windowMs: 1000 * 60 * 15,
+  limit: 100,
+  message: {
+    success: false,
+    message: "Too many requests, try again later",
+  },
+});
+
+// Apply globally
+app.use(limiter);
+
+// Routes
 app.use("/api/users", authRouter);
 app.use("/api/tasks", taskRouter);
