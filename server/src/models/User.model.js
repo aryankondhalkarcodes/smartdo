4de798ff50520ac9896c62d5a9e6b2190ca062d0
Aryan Kondhalkar <aryankondhalkar16@gmail.com>
2026-07-29 13:15:00 +0530

    feat: implement user model with validation and constraints

---
+++ server/src/models/User.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      minlength: [2, "Name should be at least 2 characters"],
+      maxlength: [40, "Name cannot exceed 40 characters"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password should be at least 8 characters"],
+      maxlength: [64, "Password cannot exceed 64 characters"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT;
 
-connectDB();
+await connectDB();
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
